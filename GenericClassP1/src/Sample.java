2e21317e5db4b31f0402da88ae39d0d539fdb10f
OnlineTrainer <mohamedtechdata@gmail.com>
2023-01-25 23:03:44 +0530

    25-01-23

---
+++ GenericClassP1/src/Sample.java
@@ -0,0 +1,27 @@
+class Demo<M>
+{
+	public M t;
+	public void setValue(M t)//public void setValue(int t)
+	{
+		this.t=t;
+	}
+	public void get()
+	{
+		System.out.println("This is get value is::"+t);
+	}
+}
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		Demo<Integer>h1=new Demo<Integer>();
+		Demo<String>h2=new Demo<String>();
+		h1.setValue(1001);
+		h1.get();
+		
+		h2.setValue("Azar");
+		h2.get();
+
+	}
+
+}
---
+++ ThisKeyword/src/Sample.java
@@ -0,0 +1,22 @@
+class Demo
+{
+	int a1,b1;
+	void get1(int pid,int cid)
+	{
+		this.a1=pid;
+		this.b1=cid;
+		System.out.println("Your value is::"+pid+""+cid);
+	}
+	void get2()
+	{
+		System.out.println("Your value is::"+a1+""+b1);
+	}
+}
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
