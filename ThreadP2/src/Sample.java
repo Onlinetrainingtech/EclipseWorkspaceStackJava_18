0e66555592b7be888319d256209c80482c2f6e9d
OnlineTrainer <mohamedtechdata@gmail.com>
2023-02-04 23:56:34 +0530

    04-02-23

--- ThreadP2/src/Sample.java
+++ ThreadP2/src/Sample.java
@@ -2,7 +2,18 @@ class Demo extends Thread
 {
 	public void run()
 	{
+		try
+		{
+		System.out.println("ThreadIn");
+		Thread.sleep(10000);
+		System.out.println("ThreadOut");
 		System.out.println("Your current thread::"+Thread.currentThread().getName());
+		System.out.println("Your Thread Priority::"+Thread.currentThread().getPriority());
+		}
+		catch(InterruptedException t)
+		{
+			
+		}
 	}
 }
 public class Sample {
@@ -12,7 +23,10 @@ public class Sample {
 		Demo t1=new Demo();
 		Demo t2=new Demo();
 		t1.start();
+		t1.setName("GameOver");
 		t2.start();
+		t2.setPriority(Thread.MAX_PRIORITY);
+		System.out.println(t1.isAlive());
 
 	}
 
